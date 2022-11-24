2f68a9158122457fa1710414eefa505d5372152c
YuYan Peng <75231933+Rumors-bitch@users.noreply.github.com>
2022-11-24 14:57:46 +0800

    ok

--- conversion.js
+++ conversion.js
@@ -3,18 +3,16 @@ async function operator(proxies = []) {
     const regex = /tcp/gi
 
     return proxies.map((proxy = {}) => {
-
-        if (targetPlatform === "Clash") {
-            if (proxy.name.match(regex) && !"ws" in proxies) {
-                _.set(proxy, 'network', 'http')
-                _.set(proxy, 'http-opts.headers.method', 'GET')
-                _.set(proxy, 'http-opts.path', ['/'])
-                _.set(proxy, 'http-opts.headers.Host', ['a.189.cn'])
-            }
-            _.set(proxy, 'network', 'ws')
-            _.set(proxy, 'ws-opts.path', '/')
-            _.set(proxy, 'ws-opts.headers.Host', 'a.189.cn')
+        if (proxy.name.match(regex) && !"ws" in proxies) {
+            _.set(proxy, 'network', 'http')
+            _.set(proxy, 'http-opts.headers.method', 'GET')
+            _.set(proxy, 'http-opts.path', ['/'])
+            _.set(proxy, 'http-opts.headers.Host', ['a.189.cn'])
         }
+        _.set(proxy, 'network', 'ws')
+        _.set(proxy, 'ws-opts.path', '/')
+        _.set(proxy, 'ws-opts.headers.Host', 'a.189.cn')
+
         return proxy;
     });
 }
